042ccb97001d05eb0fa28c5dbb5d81c8ebaf31a6
OuO333333 <37506309+OuO333333@users.noreply.github.com>
2024-01-24 17:06:55 +0800

    Update Merge k Sorted Lists.cpp

--- Merge k Sorted Lists.cpp
+++ Merge k Sorted Lists.cpp
@@ -1,55 +1,40 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        // https://www.cnblogs.com/grandyang/p/4606710.html
-        // devide and conquer
-        if(lists.empty() == true)
-            return NULL;
-        int n = lists.size();
-        while(n > 1){
-            int k = (n + 1) / 2;
-            for(int i = 0; i < n / 2; i++)
-                lists[i] = mergeTwoLists(lists[i], lists[i + k]);
-            n = k;
-        }
-        return lists[0];
+        if (lists.empty()) return NULL;
+        return mergeKLists(lists, 0, lists.size() - 1);
+    }
+
+    // Divide and Conquer
+    // https://hackmd.io/@sysprog/c-linked-list
+    ListNode* mergeKLists(vector<ListNode*>& lists, int left, int right) {
+        if (left == right) return lists[left];
+        int mid = (left + right) / 2;
+        ListNode* leftList = mergeKLists(lists, left, mid);
+        ListNode* rightList = mergeKLists(lists, mid + 1, right);
+        return mergeTwoLists(leftList, rightList);
     }
 
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(list1 == NULL)
-            return list2;
-        if(list2 == NULL)
-            return list1;
-        ListNode* res = new ListNode();
+        if (!list1) return list2;
+        if (!list2) return list1;
+        ListNode* res = new ListNode(0); // 使用0作为初始值
         ListNode* temp = res;
-        while(list1 && list2){
-            if(list1 -> val > list2 -> val){
-                temp -> next = list2;
-                list2 = list2 -> next; 
+        while (list1 && list2) {
+            if (list1->val > list2->val) {
+                temp->next = list2;
+                list2 = list2->next; 
+            } else {
+                temp->next = list1;
+                list1 = list1->next;
             }
-            else{
-                temp -> next = list1;
-                list1 = list1 ->next;
-            }
-            temp = temp -> next;
+            temp = temp->next;
         }
-        if(list1){
-            temp -> next = list1;
+        if (list1) {
+            temp->next = list1;
+        } else {
+            temp->next = list2;
         }
-        else{
-            temp -> next = list2;
-        }
-        return res -> next;
-
+        return res->next;
     }
 };
